0968a11792316da8a68332f3da0dec8ebdfdb62f
gaurav-binjola <gauravsom9266@gmail.com>
2023-09-25 09:20:34 +0530

    scrollbar updated

--- src/components/LeftNav.jsx
+++ src/components/LeftNav.jsx
@@ -25,11 +25,11 @@ const LeftNav = () => {
 
   return (
       <div
-          className={`md:block w-[240px] overflow-y-auto h-full py-4 bg-black absolute md:relative z-10  md:translate-x-0 transition-all ${
+          className={`md:block w-[240px] overflow-y-auto h-full py-4 scrollbar-hide bg-black absolute md:relative z-10  md:translate-x-0 transition-all ${
               mobileMenu ? "translate-x-0" : "translate-x-[-240px]"
           }`}
       >
-          <div className="flex px-5 flex-col">
+          <div className="flex px-5 flex-col ">
               {categories.map((item) => {
                   return (
                       <React.Fragment key={item.name}>
